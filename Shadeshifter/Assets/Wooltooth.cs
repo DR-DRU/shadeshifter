f5e0130391384af9d92b7a928f0b48ce3494a549
DR-DRU <negoescu@gmx.de>
2022-08-22 00:55:40 +0200

    Wooltooth (WIP)

--- Shadeshifter/Assets/Wooltooth.cs
+++ Shadeshifter/Assets/Wooltooth.cs
@@ -9,35 +9,60 @@ public class Wooltooth : MonoBehaviour
 {
     public Vector2 leftPatrolPoint;
     public Vector2 rightPatrolPoint;
+    Vector2 currentDestination;
+
     public float waitDuration;
-    public float sightDistance;
+    float waitingTimer;   
+    bool waiting;
 
     public float patrolSpeed;
+
     public float chaseSpeed;
-
-
     public BoxCollider2D chaseArea;
-    public BoxCollider2D searchArea;
-    public float resetDuration;
+    bool chasing;
+    GameObject chasedPlayer;
 
-    Vector2 currentDestination;
-    bool waiting;
-    float waitingTimer;
+    public float resetDuration;
+    float resetTimer; 
+    bool waitingForTargetToReEnter;
+    float exitDirection;
+
+    Rigidbody2D myRigidbody;
+
+ 
 
     // Start is called before the first frame update
     void Start()
     {
+        myRigidbody = GetComponent<Rigidbody2D>();
         currentDestination = rightPatrolPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    void CheckForTurn()
+    {
+        if (myRigidbody.velocity.x > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        else
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
     }
 
     private void FixedUpdate()
     {
+        if (chasing)
+        {
+            Chase();
+            return;
+        }
         if (waiting)
         {
             waitingTimer += Time.deltaTime;
@@ -54,8 +79,6 @@ public class Wooltooth : MonoBehaviour
                 }
                 waiting = false;
             }
-
-
         }
         else
         {
@@ -64,19 +87,65 @@ public class Wooltooth : MonoBehaviour
         
     }
 
+    void Chase()
+    {
+        if (waitingForTargetToReEnter)
+        {
+            //Check for Reset
+            myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
+            resetTimer += Time.deltaTime;
+            if (resetTimer > resetDuration)
+            {
+                chasing = false;
+                waitingForTargetToReEnter = false;
+            }
+
+            //Check if Player has reentered
+            float currentDirectionOfPlayer = chasedPlayer.transform.position.x - transform.position.x;
+
+            if (currentDirectionOfPlayer * exitDirection < 0)
+            {
+                waitingForTargetToReEnter = false;
+            }
+        }
+        else
+        {
+            if (chasedPlayer.transform.position.x > transform.position.x)
+            {
+                myRigidbody.velocity = new Vector2(chaseSpeed, myRigidbody.velocity.y);
+            }
+            else
+            {
+                myRigidbody.velocity = new Vector2(-chaseSpeed, myRigidbody.velocity.y);
+            }
+            CheckForTurn();
+        }
+    }
+
     void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position, currentDestination, patrolSpeed);
+        //transform.position = Vector2.MoveTowards(transform.position, currentDestination, patrolSpeed);
 
-        if (Vector2.Distance(transform.position, currentDestination) < 0.1f)
+
+        if (transform.position.x < currentDestination.x)
         {
+            myRigidbody.velocity = new Vector2(patrolSpeed, myRigidbody.velocity.y);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(-patrolSpeed, myRigidbody.velocity.y);
+        }
+        
+
+        //Arrived at Destination
+        if (Mathf.Abs(transform.position.x - currentDestination.x) < 0.1f)
+        {
+            myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
             waiting = true;
             waitingTimer = 0;
         }
 
     }
-
-
     void CheckForPlayer()
     {
         //RaycastHit2D leftCheck = Physics2D.Raycast(new Vector2(-width / 2 + transform.position.x, transform.position.y), Vector2.down, 0.82f, groundLayer);
@@ -85,7 +154,6 @@ public class Wooltooth : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-
         Gizmos.DrawWireSphere(leftPatrolPoint, 0.3f);
         Gizmos.DrawWireSphere(rightPatrolPoint, 0.3f);
     }
@@ -94,8 +162,19 @@ public class Wooltooth : MonoBehaviour
     {
         if (collision.gameObject.name.Equals("TestChaseArea"))
         {
-            Debug.Log("Hey");
+            waitingForTargetToReEnter = true;
+            resetTimer = 0;
+            myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
+            exitDirection = chasedPlayer.transform.position.x - transform.position.x;
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            chasing = true;
+            chasedPlayer = collision.gameObject;
+        }
+    }
 }
