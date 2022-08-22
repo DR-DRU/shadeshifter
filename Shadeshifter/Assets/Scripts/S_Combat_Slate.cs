03395cf70b2bd4fbd1ce7908ec016a91411a4438
UnrealGaimeDev <LarsKalthoff@web.de>
2022-08-22 21:18:27 +0200

    Attack combo for Slate

--- Shadeshifter/Assets/Scripts/S_Combat_Slate.cs
+++ Shadeshifter/Assets/Scripts/S_Combat_Slate.cs
@@ -21,6 +21,9 @@ public class S_Combat_Slate : S_Combat
 
     protected override void OnStartAttack(int attackIndex)
     {
+        CancelInvoke("ResetAnimation");
+        CancelInvoke("ResetCombo");
+        
         attackStatus = nextAttackStatus;
         nextAttackStatus++;
 
@@ -29,16 +32,22 @@ public class S_Combat_Slate : S_Combat
             nextAttackStatus = 1;
         }
 
-        //myAnimator.SetInteger("AttackStatus", attackStatus);
+        myAnimator.SetInteger("AttackStatus", attackStatus);
 
         S_CameraManager.Instance.ShakeCamera(0.1f, 0.5f, Cinemachine.CinemachineImpulseDefinition.ImpulseShapes.Rumble, new Vector3(-1f, -1f, 0f));
 
-        //Invoke("ResetAnimation", attacks[attackIndex].duration);
+        Invoke("ResetAnimation", attacks[attackIndex].duration);
+        Invoke("ResetCombo", attacks[attackIndex].comboTimeframe);
     }
 
     private void ResetAnimation()
     {
         attackStatus = 0;
         myAnimator.SetInteger("AttackStatus", attackStatus);
+    }
+
+    private void ResetCombo()
+    {
+        nextAttackStatus = 1;
     }
 }
